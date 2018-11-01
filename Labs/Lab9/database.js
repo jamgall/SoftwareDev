83b272233e28c8ef06b9f2172cac4011684e48bf
James Gallmeister <jgallmeister@Jamess-MacBook-Pro-2.local>
2018-11-01 14:00:34 -0600

    Completed Lab9

---
+++ Labs/Lab9/database.js
@@ -0,0 +1,12 @@
+var pgp = require('pg-promise')();
+
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'lab6',
+	user: 'postgres',
+	password: 'password'
+};
+
+var db = pgp(dbConfig);
+module.exports = db;

cc8a66c0f1c5f73c07589b4aa858f895bb014da9
James Gallmeister <jgallmeister@Jamess-MacBook-Pro-2.local>
2018-11-09 11:04:59 -0700

    added changes to ignore node_modules

--- Labs/Lab9/database.js
+++ Labs/Lab9/database.js
@@ -1,12 +1,6 @@
 var pgp = require('pg-promise')();
 
-const dbConfig = {
-	host: 'localhost',
-	port: 5432,
-	database: 'lab6',
-	user: 'postgres',
-	password: 'password'
-};
+const dbConfig = process.env.DATABASE_URL;
 
 var db = pgp(dbConfig);
 module.exports = db;
